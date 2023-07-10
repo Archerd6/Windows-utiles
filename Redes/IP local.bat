4942536b06793078a067115f0d305a38eb86d687
Archerd6 <archerd@uma.es>
2023-07-10 19:31:19 +0200

    Get IP cleaner

--- Redes/IP local.bat
+++ Redes/IP local.bat
@@ -1,7 +1,5 @@
 REM Ejecutable que muestra la IP local
 
-echo off
-
 ipconfig | find "IPv4"
 
 cmd /k
--- Redes/IP publica.bat
+++ Redes/IP publica.bat
@@ -1,7 +1,5 @@
-REM Ejecutable que muestra la IP local
+REM Ejecutable que muestra la IP publica
 
-echo off
-
-nslookup myip.opendns.com resolver1.opendns.com
+powershell -Command "nslookup myip.opendns.com resolver1.opendns.com | Select-String 'Address:' | Select-Object -Last 1"
 
 cmd /k
